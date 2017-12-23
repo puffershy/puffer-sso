9b40c71d1b3d5f8929bf725f1577ae1aa83c6ea0
buyi <9440979@qq.com>
2017-12-23 15:43:16 +0800

    U 删除无用代码

--- buyi-sso-client-pac4j-1/src/main/java/com/buyi/config/ShiroConfiguration.java
+++ buyi-sso-client-pac4j-1/src/main/java/com/buyi/config/ShiroConfiguration.java
@@ -3,25 +3,28 @@ package com.buyi.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.Filter;
 
 import org.apache.shiro.mgt.SubjectFactory;
 import org.apache.shiro.realm.Realm;
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.spring.web.config.AbstractShiroWebFilterConfiguration;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.pac4j.cas.client.CasClient;
+import org.pac4j.cas.config.CasConfiguration;
+import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.DelegatingFilterProxy;
 
 import io.buji.pac4j.filter.CallbackFilter;
+import io.buji.pac4j.filter.LogoutFilter;
 import io.buji.pac4j.filter.SecurityFilter;
+import io.buji.pac4j.realm.Pac4jRealm;
+import io.buji.pac4j.subject.Pac4jSubjectFactory;
 
 /**
  * shiro配置
@@ -31,7 +34,140 @@ import io.buji.pac4j.filter.SecurityFilter;
  * @date 2017下午3:28:02
  */
 @Configuration
-public class ShiroConfiguration{
+@EnableConfigurationProperties(ShiroProperties.class)
+public class ShiroConfiguration {
+
+	@Resource
+	private ShiroProperties properties;
+
+	/************************* pac4j cas 配置 begin ************************/
+	@Bean
+	public Realm realm() {
+		return new Pac4jRealm();
+	}
+
+	/**
+	 * cas服务的基本设置，包括或url等等，rest调用协议等
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午3:09:35
+	 * @return
+	 */
+	@Bean
+	public CasConfiguration casConfiguration() {
+		CasConfiguration casConfiguration = new CasConfiguration();
+		casConfiguration.setLoginUrl(properties.getCasLoginUrl());
+		casConfiguration.setPrefixUrl(properties.getCasServerUrlPrefix());
+		// 默认走CasProtocol.CAS30协议
+		// casConfiguration.setProtocol(CasProtocol.CAS20);
+		return casConfiguration;
+	}
+
+	/**
+	 * 定义cas客户端
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午3:09:26
+	 * @param casConfiguration
+	 * @return
+	 */
+	@Bean
+	public CasClient casClient() {
+		CasClient casClient = new CasClient();
+		casClient.setConfiguration(casConfiguration());
+		casClient.setCallbackUrl(properties.getServerCallBack());
+		// 设置cas客户端名称为cas
+		casClient.setName("cas");
+		return casClient;
+	}
+
+	/**
+	 * 定义cas客户端集合
+	 * <p>
+	 * 后期如果需要支持rest接口单点登录，就在这里扩展
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午3:20:10
+	 * @param casClient
+	 * @return
+	 */
+	@Bean
+	public Clients clients() {
+		Clients clients = new Clients();
+
+		clients.setClients(casClient());
+		clients.setDefaultClient(casClient());
+		return clients;
+	}
+
+	/**
+	 * 定义配置
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午3:22:53
+	 * @param clients
+	 * @return
+	 */
+	@Bean
+	public Config casConfig() {
+		Config config = new Config();
+		config.setClients(clients());
+
+		return config;
+	}
+
+	/**
+	 * 使用cas代理了用户，所以必须通过pac4j cas进行创建对象
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午3:30:08
+	 * @return
+	 */
+	@Bean
+	public SubjectFactory subjectFactory() {
+		return new Pac4jSubjectFactory();
+	}
+
+	/**
+	 * cas核心过滤器，把支持的client写上，filter过滤时才会处理，clients必须在casConfig.clients已经注册
+	 * <p>
+	 * 注意：该过滤器不要用spring注入，否则该过滤器会拦截所有的路径请求，除非自定义匹配规则
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午2:53:21
+	 * @return
+	 */
+	public Filter casSecurityFilter() {
+		SecurityFilter filter = new SecurityFilter();
+		filter.setClients("cas");
+		filter.setConfig(casConfig());
+		return filter;
+	}
+
+	/**
+	 * 定义cas回调过滤器
+	 * 
+	 * @author buyi
+	 * @since 1.0.0
+	 * @date 2017下午3:25:35
+	 * @param casConfig
+	 * @return
+	 */
+	@Bean
+	public CallbackFilter callbackFilter() {
+		CallbackFilter callbackFilter = new CallbackFilter();
+		callbackFilter.setConfig(casConfig());
+
+		return callbackFilter;
+	}
+
+	/************************* pac4j cas 配置 end ************************/
 
 	/**
 	 * shiro管理器
@@ -51,65 +187,15 @@ public class ShiroConfiguration{
 		return securityManager;
 	}
 
-	/**
-	 * 注册过滤器
-	 *
-	 * @author buyi
-	 * @date 2017年12月21日下午8:23:12
-	 * @since 1.0.0
-	 * @return
-	 */
-	@Bean
-	public FilterRegistrationBean registeFilter() {
-		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-
-		DelegatingFilterProxy proxy = new DelegatingFilterProxy();
-		proxy.setTargetFilterLifecycle(true);
-		proxy.setTargetBeanName("shiroFilter");
-
-		filterRegistrationBean.setFilter(proxy);
-		return filterRegistrationBean;
-	}
-
-	/**
-	 * 定义cas回调过滤器
-	 * 
-	 * @author buyi
-	 * @since 1.0.0
-	 * @date 2017下午3:25:35
-	 * @param casConfig
-	 * @return
-	 */
-	@Bean
-	public CallbackFilter callbackFilter(Config casConfig) {
-		CallbackFilter callbackFilter = new CallbackFilter();
-		callbackFilter.setConfig(casConfig);
-
-		return callbackFilter;
-	}
-
-	@Bean(name = "shiroFilter")
-	public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager, Config config) {
+	@Bean(name = "shiroFilterFactoryBean")
+	public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager) {
 		ShiroFilterFactoryBean filterFactoryBean = new ShiroFilterFactoryBean();
-		
+
 		filterFactoryBean.setFilterChainDefinitionMap(shiroFilterChainDefinition().getFilterChainMap());
-		
+
 		filterFactoryBean.setSecurityManager(securityManager);
 
-		// 过滤器设置
-		Map<String, Filter> filters = new HashMap<String, Filter>();
-
-		SecurityFilter securityFilter = new SecurityFilter();
-		securityFilter.setClients("cas");
-		securityFilter.setConfig(config);
-
-		filters.put("casSecurityFilter", securityFilter);
-
-		CallbackFilter callbackFilter = callbackFilter(config);
-
-		filters.put("callbackFilter", callbackFilter);
-
-		filterFactoryBean.setFilters(filters);
+		filterFactoryBean.setFilters(shiroFilters());
 
 		return filterFactoryBean;
 	}
@@ -118,18 +204,27 @@ public class ShiroConfiguration{
 	public ShiroFilterChainDefinition shiroFilterChainDefinition() {
 		DefaultShiroFilterChainDefinition definition = new DefaultShiroFilterChainDefinition();
 		definition.addPathDefinition("/callback", "callbackFilter");
-
 		definition.addPathDefinition("/admin/login", "anon");
-		definition.addPathDefinition("/admin/**", "casSecurityFilter");
-		definition.addPathDefinition("/index", "anon");
-		// definition.addPathDefinition("/**", "anon");
+		definition.addPathDefinition("/admin/logout", "logoutFilter");
+		definition.addPathDefinition("/**", "casSecurityFilter");
 
 		return definition;
 	}
-	
-	@Bean(name = "lifecycleBeanPostProcessor")
-	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-		return new LifecycleBeanPostProcessor();
-	}
 
+	@Bean
+	public Map<String, Filter> shiroFilters() {
+		// 过滤器设置
+		Map<String, Filter> filters = new HashMap<>();
+		filters.put("casSecurityFilter", casSecurityFilter());
+
+		filters.put("callbackFilter", callbackFilter());
+
+		LogoutFilter logoutFilter = new LogoutFilter();
+		logoutFilter.setConfig(casConfig());
+		logoutFilter.setCentralLogout(true);
+		logoutFilter.setDefaultUrl(properties.getServerUrlPrefix());
+		filters.put("logoutFilter", logoutFilter);
+
+		return filters;
+	}
 }
