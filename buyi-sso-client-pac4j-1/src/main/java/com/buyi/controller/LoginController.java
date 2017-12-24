c9a5df96b95f1f3a237fe434b7ccc1cb3c38c837
buyi <9440979@qq.com>
2017-12-24 22:43:55 +0800

    A 新增/previlige/no

--- buyi-sso-client-pac4j-1/src/main/java/com/buyi/controller/LoginController.java
+++ buyi-sso-client-pac4j-1/src/main/java/com/buyi/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.buyi.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,20 +9,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
 	@GetMapping("/admin/login")
 	public String login() {
-
 		return "admin/login";
 	}
 
 	@RequestMapping(value = { "/admin/welcome" }, method = RequestMethod.GET)
 	public String welcome() {
-
 		return "admin/welcome";
 	}
 
-	@RequestMapping(value = { "/admin/logout" }, method = RequestMethod.GET)
+	@GetMapping("/admin/logout")
 	public String logout() {
-		return "admin/logout";
+		Subject subject = SecurityUtils.getSubject();
+		subject.logout();
+		return redirect("admin/login");
+	}
+
+	/**
+	 * 带参重定向
+	 *
+	 * @param path
+	 * @return
+	 */
+	protected String redirect(String path) {
+		return "redirect:" + path;
+	}
+
+	@GetMapping("/previlige/no")
+	public String noPrems() {
+		return "/previlige/no";
 	}
 }
